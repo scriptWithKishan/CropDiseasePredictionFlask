8c369da3a1f9db0eaacec2bddf3b2f8d8d2b150f
scriptWithKishan <kishan.2785.2003@gmail.com>
2025-03-19 05:09:32 +0000

    Modified predict.py

--- predict.py
+++ predict.py
@@ -98,5 +98,6 @@ def predict():
             os.remove(filepath)
             app.logger.info(f"Removed temporary file: {filepath}")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+if __name__ != "__main__":
+    gunicorn_app = app
+
